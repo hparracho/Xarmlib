62b944a6c5d1e10c066593c0cdea568ea2e7e4f0
Helder Parracho <hparracho@gmail.com>
2018-03-10 17:46:12 +0000

    Startup code
    Added a minimal startup code.
    Added the heap allocation operators.
    Added leaner stubs to some functions of the libstdc++ and newlib libraries.

---
+++ source/system/heap_operators.cpp
@@ -0,0 +1,104 @@
+// ----------------------------------------------------------------------------
+// @file    heap_operators.cpp
+// @brief   Minimal implementations of the new/delete operators. Optional null
+//          stubs for malloc/free (only used if symbol CPP_NO_HEAP is defined).
+// @date    7 March 2018
+// ----------------------------------------------------------------------------
+//
+// Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
+// Copyright (c) 2018 Helder Parracho (hparracho@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// ----------------------------------------------------------------------------
+
+#include <cstdlib>
+
+
+
+
+void* operator new(std::size_t size) noexcept
+{
+    return malloc(size);
+}
+
+void* operator new[](std::size_t size) noexcept
+{
+    return malloc(size);
+}
+
+void operator delete(void* ptr) noexcept
+{
+    free(ptr);
+}
+
+void operator delete(void* ptr, std::size_t size __attribute__((unused))) noexcept
+{
+    free(ptr);
+}
+
+void operator delete[](void* ptr) noexcept
+{
+    free(ptr);
+}
+
+void operator delete[](void* ptr, std::size_t size __attribute__((unused))) noexcept
+{
+    free(ptr);
+}
+
+
+
+
+// Default placement versions of operator new.
+inline void* operator new(std::size_t size __attribute__((unused)),
+                          void* ptr) noexcept
+{
+    return ptr;
+}
+
+inline void* operator new[](std::size_t size __attribute__((unused)),
+                            void* ptr) noexcept
+{
+    return ptr;
+}
+
+// Default placement versions of operator delete.
+inline void operator delete(void* ptr __attribute__((unused)),
+                            void* place __attribute__((unused))) noexcept
+{}
+
+inline void operator delete[](void* ptr __attribute__((unused)),
+                              void* place __attribute__((unused))) noexcept
+{}
+
+
+
+
+#ifdef CPP_NO_HEAP
+
+extern "C" void* malloc(size_t size __attribute__((unused)))
+{
+    return nullptr;
+}
+
+extern "C" void free(void* ptr __attribute__((unused)))
+{}
+
+#endif // CPP_NO_HEAP
---
+++ source/system/libstdc++_stubs.cpp
@@ -0,0 +1,70 @@
+// ----------------------------------------------------------------------------
+// @file    libstdc++_stubs.cpp
+// @brief   Minimal implementations of the verbose terminate handler for
+//          exceptions and pure virtual functions call handler to avoid
+//          references to the heavy implementations in the standard C++ library.
+// @date    7 March 2018
+// ----------------------------------------------------------------------------
+//
+// Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
+// Copyright (c) 2018 Helder Parracho (hparracho@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// ----------------------------------------------------------------------------
+
+
+
+
+#include <stdlib.h>
+
+
+
+
+namespace __gnu_cxx
+{
+
+// ----------------------------------------------------------------------------
+// This function is called when an uncaught C++ exception is encountered. The
+// default version within the C++ library prints the name of the uncaught
+// exception, but to do so it must demangle its name - which causes a large
+// amount of code to be pulled in. The below minimal implementation can reduce
+// code size noticeably. Note that this function should not return.
+// ----------------------------------------------------------------------------
+[[noreturn]] void __verbose_terminate_handler()
+{
+    abort();
+}
+
+} // namespace __gnu_cxx
+
+
+
+
+// ----------------------------------------------------------------------------
+// This function is referenced whenever the code contains pure virtual
+// functions. This is a placeholder used while constructing an object, and
+// the function should never actually get called. The below minimal
+// implementation removes all the dependencies from original function and can
+// reduce code size noticeably. Note that this function should not return.
+// ----------------------------------------------------------------------------
+extern "C" [[noreturn]] void __cxa_pure_virtual()
+{
+    abort();
+}
