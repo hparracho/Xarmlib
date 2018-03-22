28686a2ea1103a11311583eb03ec9a0c65f4bdc1
Helder Parracho <hparracho@gmail.com>
2018-03-22 15:54:09 +0000

    LPC84x - Flash Memory Controller
    Added basic FMC support on LPC84x target.
    
    Co-Authored-By: emanuelpinto <emanuelpinto@users.noreply.github.com>

---
+++ include/targets/LPC84x/lpc84x_fmc.hpp
@@ -0,0 +1,93 @@
+// ----------------------------------------------------------------------------
+// @file    lpc84x_fmc.hpp
+// @brief   NXP LPC84x Flash Memory Controller (FMC) class.
+// @date    22 March 2018
+// ----------------------------------------------------------------------------
+//
+// Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
+// Copyright (c) 2018 Helder Parracho (hparracho@gmail.com)
+//
+// See README.md file for additional credits and acknowledgments.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// ----------------------------------------------------------------------------
+
+#ifndef __XARMLIB_TARGETS_LPC84X_FMC_HPP
+#define __XARMLIB_TARGETS_LPC84X_FMC_HPP
+
+#ifdef __LPC84X__
+
+#include <targets/LPC84x/lpc84x_cmsis.hpp>
+
+namespace xarmlib
+{
+namespace lpc84x
+{
+
+
+
+
+class Fmc
+{
+    public:
+
+        // --------------------------------------------------------------------
+        // PUBLIC DEFINITIONS
+        // --------------------------------------------------------------------
+
+        // Flash access time definitions
+        enum class AccessTime
+        {
+            TIME_1_SYSCLK = 0,      // Flash accesses use 1 system clock
+            TIME_2_SYSCLK,          // Flash accesses use 2 system clocks
+            TIME_3_SYSCLK           // Flash accesses use 3 system clocks
+        };
+
+        // --------------------------------------------------------------------
+        // PUBLIC MEMBER FUNCTIONS
+        // --------------------------------------------------------------------
+
+        // Set flash memory access time in clocks
+        static void set_access_time(const AccessTime clocks)
+        {
+            LPC_FMC->FLASHCFG = (LPC_FMC->FLASHCFG & (~0x03))
+                              | static_cast<uint32_t>(clocks);
+        }
+
+    private:
+
+        // --------------------------------------------------------------------
+        // PRIVATE MEMBER FUNCTIONS
+        // --------------------------------------------------------------------
+
+        // --------------------------------------------------------------------
+        // PRIVATE DEFINITIONS
+        // --------------------------------------------------------------------
+};
+
+
+
+
+} // namespace lpc84x
+} // namespace xarmlib
+
+#endif // __LPC84X__
+
+#endif // __XARMLIB_TARGETS_LPC84X_FMC_HPP
