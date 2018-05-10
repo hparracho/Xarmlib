8383b7a2132b0ed07de0deb8c127ad06f7e9f6d5
Helder Parracho <hparracho@gmail.com>
2018-05-10 16:02:53 +0100

    System - replaced assert macro with a proper function

--- source/system/libstdc++_stubs.cpp
+++ source/system/libstdc++_stubs.cpp
@@ -3,7 +3,7 @@
 // @brief   Minimal implementations of the verbose terminate handler for
 //          exceptions and pure virtual functions call handler to avoid
 //          references to the heavy implementations in the standard C++ library.
-// @date    21 March 2018
+// @date    10 May 2018
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
@@ -31,7 +31,9 @@
 //
 // ----------------------------------------------------------------------------
 
-#include <stdlib.h>
+// Forward declaration of abort function defined in 'newlib_stubs.cpp' file.
+extern "C" __attribute__ ((weak, noreturn))
+void abort(void);
 
 
 
