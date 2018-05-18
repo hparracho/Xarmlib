76bd7748c6f5df9095ed96c0d5ca8ba7a1356b0a
Helder Parracho <hparracho@gmail.com>
2018-05-18 11:03:53 +0100

    HAL - Watchdog - corrected file date

--- include/hal/hal_watchdog.hpp
+++ include/hal/hal_watchdog.hpp
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // @file    hal_watchdog.hpp
 // @brief   Watchdog HAL interface class.
-// @date    18 April 2018
+// @date    18 May 2018
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
