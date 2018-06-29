3942b390ca91fa2d2d843ca77f7bc0376d74f713
Emanuel Pinto <emanuel.pinto@fpelectronica.com>
2018-06-29 15:38:57 +0100

    Update hal_port.hpp

--- include/hal/hal_port.hpp
+++ include/hal/hal_port.hpp
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // @file    hal_port.hpp
 // @brief   Port HAL interface class.
-// @date    30 May 2018
+// @date    29 June 2018
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
