3becda4db01c6fbdbd1102c06b9c1ead0f0a2f84
emanuelpinto <emanuelangelopinto@gmail.com>
2018-07-11 09:45:52 +0100

    DEVICES - SpiIoPort - added another transfer function
    This new private transfer function is intended to be used only by SpiIoPortDebouncer class

--- devices/include/spi_io_port.hpp
+++ devices/include/spi_io_port.hpp
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // @file    spi_io_port.hpp
 // @brief   SPI I/O port class (based on module FPIO8SM).
-// @date    5 July 2018
+// @date    10 July 2018
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
@@ -61,8 +61,8 @@ class SpiIoPort
         // Transfer a buffer (simultaneous write and read)
         void transfer(const gsl::span<const uint8_t> output_buffer, const gsl::span<uint8_t> input_buffer)
         {
-            assert(output_buffer.size() == input_buffer.size());
             assert(output_buffer.size() > 0);
+            assert(output_buffer.size() == input_buffer.size());
 
             m_latch = 0;
             m_latch = 1;
@@ -90,6 +90,61 @@ class SpiIoPort
         bool is_enabled() const { return (m_enable == 0); }
 
     private:
+
+        friend class SpiIoPortDebouncer;
+
+        // --------------------------------------------------------------------
+        // PRIVATE MEMBER FUNCTIONS
+        // --------------------------------------------------------------------
+
+        // Transfer a buffer (simultaneous write and read)
+        // NOTE: ports are concatenated in size of uint32_t
+        void transfer(const gsl::span<const uint32_t> output_buffer, const gsl::span<InputDebouncer::PortMask> port_buffer, const std::size_t port_count)
+        {
+            assert(port_count > 0);
+            // Test the buffer size ceiled: ((port_count - 1) / 4) + 1
+            assert(output_buffer.size() == static_cast<std::ptrdiff_t>(((port_count - 1) >> 2) + 1));
+            assert(output_buffer.size() == port_buffer.size());
+
+            for(auto& port : port_buffer)
+            {
+                port.current_read = 0;
+            }
+
+            m_latch = 0;
+            m_latch = 1;
+
+            m_spi_master.mutex_take();
+
+            for(std::ptrdiff_t port_index = port_count - 1; port_index >= 0; --port_index)
+            {
+                const std::size_t buffer_index = get_buffer_index(port_index);
+                const int32_t     buffer_shift = get_buffer_shift(port_index);
+
+                const uint32_t output_port = (output_buffer[buffer_index] >> buffer_shift) & 0xFF;
+
+                const uint32_t input_port = m_spi_master.transfer(output_port) & 0xFF;
+
+                auto& port = port_buffer[buffer_index];
+
+                port.current_read |= static_cast<uint32_t>(input_port << buffer_shift);
+            }
+
+            m_spi_master.mutex_give();
+
+            m_latch = 0;
+            m_latch = 1;
+        }
+
+        static constexpr std::size_t get_buffer_index(const std::size_t port_index)
+        {
+            return static_cast<std::size_t>(port_index >> 2);       // (port_index / 4)
+        }
+
+        static constexpr int32_t get_buffer_shift(const std::size_t port_index)
+        {
+            return static_cast<int32_t>((port_index & 0x03) << 3);  // (port_index % 4) * 8 pins
+        }
 
         // --------------------------------------------------------------------
         // PRIVATE MEMBER VARIABLES
