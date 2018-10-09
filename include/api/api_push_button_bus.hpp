1c563f97b8967fde9288dec81bc331378d08fc09
emanuelpinto <emanuelangelopinto@gmail.com>
2018-10-09 10:40:10 +0100

    API - revised PushButtonBus class

--- include/api/api_push_button_bus.hpp
+++ include/api/api_push_button_bus.hpp
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // @file    api_push_button_bus.hpp
 // @brief   API push-button bus class.
-// @date    1 August 2018
+// @date    4 October 2018
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
@@ -42,16 +42,6 @@ namespace xarmlib
 
 
 
-struct PushButtonData
-{
-    int16_t index   { -1 };
-    bool    coded   { false };
-    bool    enabled { false };
-};
-
-
-
-
 template <class Type, class Enable = void>
 class PushButtonBus;
 
@@ -62,129 +52,144 @@ class PushButtonBus<Type, typename std::enable_if<std::is_same<Type, Pin::Name>:
     public:
 
         // --------------------------------------------------------------------
+        // PUBLIC DEFINITIONS
+        // --------------------------------------------------------------------
+
+        struct Data
+        {
+            Type     pin;
+            uint8_t  binary1 { 0 };
+            uint32_t code    { 0 }; // NOTES: - 0 means no code
+                                    //        - code is based on 4 binary1 digits (4 bytes): digit4 | digit3 | digit2 | digit1
+            bool     enabled { false };
+            bool     stage2_selected { false };
+        };
+
+        // --------------------------------------------------------------------
         // PUBLIC MEMBER FUNCTIONS
         // --------------------------------------------------------------------
 
-        // -------- CONSTRUCTORS ----------------------------------------------
-
         PushButtonBus() = delete;
 
-        constexpr PushButtonBus(const std::initializer_list<Type>           pin_list,
-                                const std::initializer_list<PushButtonData> data_list) : m_pin_list(pin_list),
-                                                                                         m_data_list(data_list),
-                                                                                         m_list_count { pin_list.size() }
-        {
-#ifndef NDEBUG
-            assert(pin_list.size() == data_list.size());
-            assert(pin_list.size() <= 32);
-
-            for(const auto pin : pin_list)
-            {
-                if constexpr(std::is_same<Type, Pin::Name>::value == true)
-                {
-                    assert(pin != Pin::Name::NC);
-                }
-
-                if constexpr(std::is_same<Type, int8_t>::value == true)
-                {
-                    assert(pin >= 0);
-                }
-            }
-
-            for(const auto data : data_list)
-            {
-                assert(data.index > 0);
-            }
-#endif
-        }
-
-        constexpr PushButtonBus(const std::size_t count) : m_pin_list(count),
-                                                           m_data_list(count),
-                                                           m_list_count { 0 }
+        constexpr PushButtonBus(const std::size_t count) : m_push_button_list(count),
+                                                           m_push_button_list_size { 0 }
         {
             assert(count <= 32);
         }
 
-        // -------- ASSIGNMENT FUNCTION ---------------------------------------
+        // -------- APPEND PUSH-BUTTON FUNCTION -------------------------------
 
-        constexpr void append(const Type*           pin_list_begin,  const Type*           pin_list_end,
-                              const PushButtonData* data_list_begin, const PushButtonData* data_list_end)
+        constexpr void append(const Data data)
         {
-            const auto pin_list_size = std::distance(pin_list_begin, pin_list_end);
-
 #ifndef NDEBUG
-            assert(pin_list_size == std::distance(data_list_begin, data_list_end));
-            assert(m_list_count + pin_list_size <= m_pin_list.size());
+            assert(m_push_button_list_size + 1 <= m_push_button_list.size());
 
-            for(auto i = 0; i < pin_list_size; ++i)
+            if constexpr(std::is_same<Type, Pin::Name>::value == true)
             {
-                if constexpr(std::is_same<Type, Pin::Name>::value == true)
-                {
-                    assert(pin_list_begin[i] != Pin::Name::NC);
-                }
-
-                if constexpr(std::is_same<Type, int8_t>::value == true)
-                {
-                    assert(pin_list_begin[i] >= 0);
-                }
-
-                assert(data_list_begin[i].index > 0);
+                assert(data.pin != Pin::Name::NC);
             }
+
+            if constexpr(std::is_same<Type, int8_t>::value == true)
+            {
+                assert(data.pin >= 0);
+            }
+
+            assert(data.binary1 > 0);
 #endif
 
-            std::copy(pin_list_begin,  pin_list_end,  &m_pin_list[m_list_count]);
-            std::copy(data_list_begin, data_list_end, &m_data_list[m_list_count]);
+            m_push_button_list[m_push_button_list_size] = data;
 
-            m_list_count += pin_list_size;
+            m_push_button_list_size++;
         }
 
-        // -------- DATA ACCESS FUNCTIONS / OPERATORS -------------------------
+        // -------- GET/SET DATA MEMBERS FUNCTIONS ----------------------------
 
-        constexpr const PushButtonData* get_data_list() const
+        constexpr uint8_t get_binary1(const std::size_t index) const
         {
-            return m_data_list.data();
+            assert(index < m_push_button_list_size);
+
+            return m_push_button_list[index].binary1;
         }
 
-        PushButtonData& operator [] (const std::size_t pos)
+        constexpr void set_binary1(const std::size_t index, const uint8_t binary1)
         {
-            assert(pos < m_list_count);
+            assert(index < m_push_button_list_size);
+            assert(binary1 > 0);
 
-            return m_data_list[pos];
+            m_push_button_list[index].binary1 = binary1;
         }
 
-        constexpr const PushButtonData& operator [] (const std::size_t pos) const
+        constexpr uint32_t get_code(const std::size_t index) const
         {
-            assert(pos < m_list_count);
+            assert(index < m_push_button_list_size);
 
-            return m_data_list[pos];
+            return m_push_button_list[index].code;
+        }
+
+        constexpr void set_code(const std::size_t index, const uint32_t code)
+        {
+            assert(index < m_push_button_list_size);
+
+            m_push_button_list[index].code = code;
+        }
+
+        constexpr bool get_enabled(const std::size_t index) const
+        {
+            assert(index < m_push_button_list_size);
+
+            return m_push_button_list[index].enabled;
+        }
+
+        constexpr void set_enabled(const std::size_t index, const bool enabled)
+        {
+            assert(index < m_push_button_list_size);
+
+            m_push_button_list[index].enabled = enabled;
+        }
+
+        constexpr bool get_stage2_selected(const std::size_t index) const
+        {
+            assert(index < m_push_button_list_size);
+
+            return m_push_button_list[index].stage2_selected;
+        }
+
+        constexpr void set_stage2_selected(const std::size_t index, const bool stage2_selected)
+        {
+            assert(index < m_push_button_list_size);
+
+            m_push_button_list[index].stage2_selected = stage2_selected;
         }
 
         // -------- CAPACITY FUNCTION -----------------------------------------
 
+        constexpr bool is_empty() const
+        {
+            return (m_push_button_list_size == 0);
+        }
+
         constexpr std::size_t get_size() const
         {
-            return m_list_count;
+            return m_push_button_list_size;
         }
 
         // -------- MASK FUNCTION ---------------------------------------------
 
         constexpr uint32_t get_mask() const
         {
-            assert(m_list_count > 0);
-
             return static_cast<uint32_t>((1UL << get_size()) - 1);
         }
 
-        // -------- PIN ITERATORS ---------------------------------------------
+        // -------- ITERATORS -------------------------------------------------
 
-        constexpr const Type* begin() const
+        constexpr const Data* begin() const
         {
-            return m_pin_list.begin();
+            return m_push_button_list.begin();
         }
 
-        constexpr const Type* end() const
+        constexpr const Data* end() const
         {
-            return m_pin_list.end();
+            return m_push_button_list.begin() + get_size();
         }
 
     private:
@@ -193,9 +198,8 @@ class PushButtonBus<Type, typename std::enable_if<std::is_same<Type, Pin::Name>:
         // PRIVATE MEMBER VARIABLES
         // --------------------------------------------------------------------
 
-        std::dynarray<Type>           m_pin_list;
-        std::dynarray<PushButtonData> m_data_list;
-        std::size_t                   m_list_count;
+        std::dynarray<Data> m_push_button_list;
+        std::size_t         m_push_button_list_size;
 };
 
 
