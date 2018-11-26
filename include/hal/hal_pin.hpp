57a215488a89d67e386352b3fa97199c3ab989e5
emanuelpinto <emanuelangelopinto@gmail.com>
2018-11-26 10:45:19 +0000

    HAL - Pin - removed temporary definitions and functions only used for tests

--- include/hal/hal_pin.hpp
+++ include/hal/hal_pin.hpp
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // @file    hal_pin.hpp
 // @brief   Pin HAL interface class.
-// @date    20 November 2018
+// @date    22 November 2018
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
@@ -49,36 +49,7 @@ class Pin
         // PUBLIC TYPE ALIASES
         // --------------------------------------------------------------------
 
-        using Name         = typename TargetPin::Name;
-        using FunctionMode = typename TargetPin::FunctionMode;
-
-        //TMP
-        using SlewRate        = typename TargetPin::SlewRate;
-        using PassiveFilter   = typename TargetPin::PassiveFilter;
-        using OpenDrain       = typename TargetPin::OpenDrain;
-        using DriveStrength   = typename TargetPin::DriveStrength;
-        using PinMuxControl   = typename TargetPin::PinMuxControl;
-        using LockRegister    = typename TargetPin::LockRegister;
-
-        static void set_mode(const Name pin_name, const FunctionMode    function_mode,
-                                                  const PinMuxControl   pin_mux_control,
-                                                  const SlewRate        slew_rate      = SlewRate::kPORT_FastSlewRate,
-                                                  const PassiveFilter   passive_filter = PassiveFilter::kPORT_PassiveFilterDisable,
-                                                  const OpenDrain       open_drain     = OpenDrain::kPORT_OpenDrainDisable,
-                                                  const DriveStrength   drive_strength = DriveStrength::kPORT_LowDriveStrength,
-                                                  const LockRegister    lock_register  = LockRegister::kPORT_UnlockRegister)
-        {
-            TargetPin::set_mode(pin_name, function_mode, pin_mux_control, slew_rate, passive_filter, open_drain, drive_strength, lock_register);
-        }
-
-        static void set_mode(const Name pin_name, const PinMuxControl   pin_mux_control = PinMuxControl::kPORT_MuxAlt7, // I2C0 configuration
-                                                  const SlewRate        slew_rate       = SlewRate::kPORT_FastSlewRate,
-                                                  const PassiveFilter   passive_filter  = PassiveFilter::kPORT_PassiveFilterDisable,
-                                                  const DriveStrength   drive_strength  = DriveStrength::kPORT_LowDriveStrength,
-                                                  const LockRegister    lock_register   = LockRegister::kPORT_UnlockRegister)
-        {
-            TargetPin::set_mode(pin_name, pin_mux_control, slew_rate, passive_filter, drive_strength, lock_register);
-        }
+        using Name = typename TargetPin::Name;
 };
 
 
