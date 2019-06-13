eedbcd843834e0872e7db93222ff51eb2fad93d4
emanuelpinto <emanuelangelopinto@gmail.com>
2019-06-13 17:05:40 +0100

    LPC81x - renamed iap driver class

--- include/targets/LPC81x/lpc81x_flash_iap.hpp
+++ include/targets/LPC81x/lpc81x_flash_iap.hpp
@@ -1,11 +1,11 @@
 // ----------------------------------------------------------------------------
-// @file    lpc81x_iap.hpp
-// @brief   NXP LPC81x In-Application Programming (IAP) class.
-// @date    9 April 2019
+// @file    lpc81x_flash_iap.hpp
+// @brief   NXP LPC81x flash In-Application Programming (IAP) class.
+// @date    21 May 2019
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
-// Copyright (c) 2018 Helder Parracho (hparracho@gmail.com)
+// Copyright (c) 2018-2019 Helder Parracho (hparracho@gmail.com)
 //
 // See README.md file for additional credits and acknowledgments.
 //
@@ -29,11 +29,11 @@
 //
 // ----------------------------------------------------------------------------
 
-#ifndef __XARMLIB_TARGETS_LPC81X_IAP_HPP
-#define __XARMLIB_TARGETS_LPC81X_IAP_HPP
+#ifndef __XARMLIB_TARGETS_LPC81X_FLASH_IAP_HPP
+#define __XARMLIB_TARGETS_LPC81X_FLASH_IAP_HPP
 
 #include "external/span.hpp"
-#include "targets/LPC81x/lpc81x_pin.hpp"
+#include "targets/LPC81x/lpc81x_cmsis.hpp"
 
 namespace xarmlib
 {
@@ -45,38 +45,9 @@ namespace lpc81x
 
 
 
-class IapDriver
+class FlashIapDriver
 {
     public:
-
-        // --------------------------------------------------------------------
-        // PUBLIC DEFINITIONS
-        // --------------------------------------------------------------------
-
-        // IAP status codes
-        enum class StatusCode
-        {
-            cmd_success = 0,                            // Command is executed successfully
-            invalid_command,                            // Invalid command
-            src_addr_error,                             // Source address not on word boundary
-            dst_addr_error,                             // Destination address is not on a correct boundary
-            src_addr_not_mapped,                        // Source address is not mapped in the memory map
-            dst_addr_not_mapped,                        // Destination address is not mapped in the memory map
-            count_error,                                // Byte count is not multiple of 4 or is not a permitted value
-            invalid_sector_invalid_page,                // Sector/page number is invalid or end sector number is greater than start sector number
-            sector_not_blank,                           // Sector is not blank
-            sector_not_prepared_for_write_operation,    // Command to prepare sector for write operation was not executed
-            compare_error,                              // Source and destination data not equal
-            busy,                                       // Flash programming hardware interface is busy
-            param_error,                                // Insufficient number of parameters or invalid parameter
-            addr_error,                                 // Address is not on word boundary
-            addr_not_mapped,                            // Address is not mapped in the memory map
-            cmd_locked,                                 // Command is locked
-            invalid_code,                               // Unlock code is invalid
-            invalid_baud_rate,                          // Invalid baud rate setting
-            invalid_stop_bit,                           // Invalid stop bit setting
-            code_read_protection_enabled                // Code read protection enabled
-        };
 
         // --------------------------------------------------------------------
         // PUBLIC MEMBER FUNCTIONS
@@ -163,6 +134,50 @@ class IapDriver
     private:
 
         // --------------------------------------------------------------------
+        // PRIVATE DEFINITIONS
+        // --------------------------------------------------------------------
+
+        // IAP status codes
+        enum class StatusCode
+        {
+            cmd_success = 0,                            // Command is executed successfully
+            invalid_command,                            // Invalid command
+            src_addr_error,                             // Source address not on word boundary
+            dst_addr_error,                             // Destination address is not on a correct boundary
+            src_addr_not_mapped,                        // Source address is not mapped in the memory map
+            dst_addr_not_mapped,                        // Destination address is not mapped in the memory map
+            count_error,                                // Byte count is not multiple of 4 or is not a permitted value
+            invalid_sector_invalid_page,                // Sector/page number is invalid or end sector number is greater than start sector number
+            sector_not_blank,                           // Sector is not blank
+            sector_not_prepared_for_write_operation,    // Command to prepare sector for write operation was not executed
+            compare_error,                              // Source and destination data not equal
+            busy,                                       // Flash programming hardware interface is busy
+            param_error,                                // Insufficient number of parameters or invalid parameter
+            addr_error,                                 // Address is not on word boundary
+            addr_not_mapped,                            // Address is not mapped in the memory map
+            cmd_locked,                                 // Command is locked
+            invalid_code,                               // Unlock code is invalid
+            invalid_baud_rate,                          // Invalid baud rate setting
+            invalid_stop_bit,                           // Invalid stop bit setting
+            code_read_protection_enabled                // Code read protection enabled
+        };
+
+        // Command codes for IAP
+        enum class CommandCode
+        {
+            prepare_sector     = 50,
+            copy_ram_to_flash  = 51,
+            erase_sector       = 52,
+            blank_check_sector = 53,
+            read_part_id       = 54,
+            read_boot_code_ver = 55,
+            compare            = 56,
+            reinvoke_isp       = 57,
+            read_uid           = 58,
+            erase_page         = 59
+        };
+
+        // --------------------------------------------------------------------
         // PRIVATE MEMBER FUNCTIONS
         // --------------------------------------------------------------------
 
@@ -238,25 +253,6 @@ class IapDriver
 
             return static_cast<StatusCode>(result[0]);
         }
-
-        // --------------------------------------------------------------------
-        // PRIVATE DEFINITIONS
-        // --------------------------------------------------------------------
-
-        // Command codes for IAP
-        enum class CommandCode
-        {
-            prepare_sector     = 50,
-            copy_ram_to_flash  = 51,
-            erase_sector       = 52,
-            blank_check_sector = 53,
-            read_part_id       = 54,
-            read_boot_code_ver = 55,
-            compare            = 56,
-            reinvoke_isp       = 57,
-            read_uid           = 58,
-            erase_page         = 59
-        };
 };
 
 
@@ -266,4 +262,4 @@ class IapDriver
 } // namespace targets
 } // namespace xarmlib
 
-#endif // __XARMLIB_TARGETS_LPC81X_IAP_HPP
+#endif // __XARMLIB_TARGETS_LPC81X_FLASH_IAP_HPP
