690c5b9f8150d12061bbfcaf66487578bcc9b240
emanuelpinto <emanuelangelopinto@gmail.com>
2019-07-25 18:08:05 +0100

    External - FATFS - diskio.cpp - moved the exclusive FatFs public functions from each volume/drive class to this file and more few changes

--- external/FATFS/diskio.cpp
+++ external/FATFS/diskio.cpp
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // @file    diskio.cpp
 // @brief   Low level disk I/O functions for FatFs.
-// @date    22 July 2019
+// @date    25 July 2019
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
@@ -29,26 +29,190 @@
 //
 // ----------------------------------------------------------------------------
 
+#include "xarmlib_config.hpp"
+
+#if (XARMLIB_ENABLE_FATFS == 1)
+
 #include "diskio.h"
-#include "devices/spi_nor_flash.hpp"
-#include "devices/spi_sd_card.hpp"
 
 namespace xarmlib
 {
 
+
+
+
+// ----------------------------------------------------------------------------
+// SPI SD CARD DISK I/O IMPLEMENTATION
+// ----------------------------------------------------------------------------
+
+// Global SD card interface
+static std::unique_ptr<SpiSdCard> g_spi_sd_card_ptr = nullptr;
+
+
+
+
+// SPI SD card class initialization
+// NOTE: should be called by the user case FatFs will be used
+bool SpiSdCard_fatfs_initialize(hal::SpiMaster& spi_master, const hal::Pin::Name cs)
+{
+    assert(g_spi_sd_card_ptr == nullptr);
+
+    g_spi_sd_card_ptr = std::make_unique<SpiSdCard>(spi_master, cs);
+
+    assert(g_spi_sd_card_ptr != nullptr);
+
+    if(g_spi_sd_card_ptr->start() == true)
+    {
+        return g_spi_sd_card_ptr->read_configuration();
+    }
+
+    return false;
+}
+
+
+
+
+// Check if memory card is initialized
+// NOTE: to be used inside disk_initialize function
+bool SpiSdCard_fatfs_is_initialized()
+{
+    return (g_spi_sd_card_ptr != nullptr);
+}
+
+// Read a single or multiple sectors from memory card
+// NOTE: to be used inside disk_read function
+bool SpiSdCard_fatfs_read_sector(uint32_t starting_sector, uint8_t* data_array, std::size_t sector_count)
+{
+    assert(g_spi_sd_card_ptr != nullptr);
+
+    return g_spi_sd_card_ptr->read_sector(starting_sector, data_array, sector_count);
+}
+
+// Write a single or multiple sectors to memory card
+// NOTE: to be used inside disk_write function
+bool SpiSdCard_fatfs_write_sector(uint32_t starting_sector, const uint8_t* data_array, std::size_t sector_count)
+{
+    assert(g_spi_sd_card_ptr != nullptr);
+
+    return g_spi_sd_card_ptr->write_sector(starting_sector, data_array, sector_count);
+}
+
+// Control device specific features and miscellaneous functions other than generic read/write
+// NOTE: to be used inside disk_ioctl function
+bool SpiSdCard_fatfs_control(const uint8_t code, void* data)
+{
+    assert(g_spi_sd_card_ptr != nullptr);
+
+    return g_spi_sd_card_ptr->control(code, data);
+}
+
+
+
+
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
+
+// ----------------------------------------------------------------------------
+// SPI NOR FLASH DISK I/O IMPLEMENTATION
+// ----------------------------------------------------------------------------
+
+// Global flash interface
+static std::unique_ptr<SpiNorFlash> g_spi_nor_flash_ptr = nullptr;
+
+
+
+
+// SPI NOR flash class initialization
+// NOTE: should be called by the user case FatFs will be used
+bool SpiNorFlash_fatfs_initialize(hal::SpiMaster& spi_master, const hal::Pin::Name cs, const hal::Pin::Name wp, const bool readonly)
+{
+    assert(g_spi_nor_flash_ptr == nullptr);
+
+    g_spi_nor_flash_ptr = std::make_unique<SpiNorFlash>(spi_master, cs, wp, readonly);
+
+    assert(g_spi_nor_flash_ptr != nullptr);
+
+    return g_spi_nor_flash_ptr->read_configuration();
+
+    //g_spi_nor_flash_ptr->erase_chip();
+}
+
+
+
+
+// Check if flash is initialized
+// NOTE: to be used inside disk_initialize function
+bool SpiNorFlash_fatfs_is_initialized()
+{
+    return (g_spi_nor_flash_ptr != nullptr);
+}
+
+// Check if flash is read-only
+// NOTE: to be used inside disk_initialize function
+bool SpiNorFlash_fatfs_is_readonly()
+{
+    assert(g_spi_nor_flash_ptr != nullptr);
+
+    return g_spi_nor_flash_ptr->is_readonly();
+}
+
+// Read a single or multiple sectors from memory flash
+// NOTE: to be used inside disk_read function
+bool SpiNorFlash_fatfs_read_sector(uint32_t starting_sector, uint8_t* data_array, std::size_t sector_count)
+{
+    assert(g_spi_nor_flash_ptr != nullptr);
+
+    return g_spi_nor_flash_ptr->read_sector(starting_sector, data_array, sector_count);
+}
+
+// Write a single or multiple sectors to memory flash
+// NOTES: to be used inside disk_write function
+//        a reading and comparison is done after each sector written, returning its result
+bool SpiNorFlash_fatfs_write_sector(uint32_t starting_sector, const uint8_t* data_array, std::size_t sector_count)
+{
+    assert(g_spi_nor_flash_ptr != nullptr);
+
+    if(g_spi_nor_flash_ptr->erase_sector(starting_sector, sector_count) == true)
+    {
+        if(g_spi_nor_flash_ptr->program_sector(starting_sector, data_array, sector_count) == true)
+        {
+            return g_spi_nor_flash_ptr->compare_sector(starting_sector, data_array, sector_count);
+        }
+    }
+
+    return false;
+}
+
+// Control device specific features and miscellaneous functions other than generic read/write
+// NOTE: to be used inside disk_ioctl function
+bool SpiNorFlash_fatfs_control(const uint8_t code, void* data)
+{
+    assert(g_spi_nor_flash_ptr != nullptr);
+
+    return g_spi_nor_flash_ptr->control(code, data);
+}
+
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
+
+
+
+
+// ----------------------------------------------------------------------------
+// FATFS DISK I/O IMPLEMENTATION
+// ---------------------------------------------------------------------------
+
 extern "C"
 {
-
-
-
-
 
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 // ----------------------------------------------------------------------------
 
 // Disk status
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
 static DSTATUS g_disk_status[2] = { STA_NOINIT, STA_NOINIT };
+#else
+static DSTATUS g_disk_status[1] = { STA_NOINIT };
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
 
 
 
@@ -63,13 +227,22 @@ DSTATUS disk_initialize(BYTE pdrv /* Physical drive number (0..) */)
 {
     switch(pdrv)
     {
+        case SDCARD:
+            if(SpiSdCard_fatfs_is_initialized() == true)
+            {
+                // Start up OK!
+                g_disk_status[SDCARD] &= ~STA_NOINIT;
+            }
+            return g_disk_status[SDCARD];
+
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
         case FLASH:
-            if(SpiNorFlash_read_configuration() == true)
+            if(SpiNorFlash_fatfs_is_initialized() == true)
             {
                 // Start up OK!
                 g_disk_status[FLASH] &= ~STA_NOINIT;
 
-                if (SpiNorFlash_is_readonly() == true)
+                if (SpiNorFlash_fatfs_is_readonly() == true)
                 {
                     // Write protected
                     g_disk_status[FLASH] |= STA_PROTECT;
@@ -80,22 +253,8 @@ DSTATUS disk_initialize(BYTE pdrv /* Physical drive number (0..) */)
                     g_disk_status[FLASH] &= ~STA_PROTECT;
                 }
             }
-            /*else if(SpiFlashAt45_IsInitialized() == true)
-            {
-                // Start up OK!
-                g_disk_status[FLASH] &= ~STA_NOINIT;
-
-                // @TODO: IMPLEMENTAR VERIFICAÇÃO DO WRITE PROTECT!!
-            }*/
             return g_disk_status[FLASH];
-
-        case SDCARD:
-            if(SpiSdCard_start() == true && SpiSdCard_read_configuration() == true)
-            {
-                // Start up OK!
-                g_disk_status[SDCARD] &= ~STA_NOINIT;
-            }
-            return g_disk_status[SDCARD];
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     }
 
     // Drive not supported
@@ -111,8 +270,10 @@ DSTATUS disk_status(BYTE pdrv /* Physical drive number (0..) */)
 {
     switch(pdrv)
     {
-        case FLASH:  return g_disk_status[FLASH];
         case SDCARD: return g_disk_status[SDCARD];
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
+        case FLASH:  return g_disk_status[FLASH];
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     }
 
     // Drive not supported
@@ -129,7 +290,11 @@ DRESULT disk_read(BYTE  pdrv,   /* Physical drive number (0..) */
                   DWORD sector, /* Sector address (LBA) */
                   UINT  count   /* Number of sectors to read (1..128) */)
 {
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     if(pdrv > 1 || count == 0)
+#else
+    if(pdrv > 0 || count == 0)
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     {
         return RES_PARERR;
     }
@@ -141,23 +306,21 @@ DRESULT disk_read(BYTE  pdrv,   /* Physical drive number (0..) */
 
     switch(pdrv)
     {
-        case FLASH:
-            if(SpiNorFlash_read_sector(sector, buff, count) == true)
+        case SDCARD:
+            if(SpiSdCard_fatfs_read_sector(sector, buff, count) == true)
             {
                 return RES_OK;
             }
-            /*else if(SpiFlashAt45_ReadSector(sector, buff, count) == true)
-            {
-                return RES_OK;
-            }*/
             break;
 
-        case SDCARD:
-            if(SpiSdCard_read_sector(sector, buff, count) == true)
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
+        case FLASH:
+            if(SpiNorFlash_fatfs_read_sector(sector, buff, count) == true)
             {
                 return RES_OK;
             }
             break;
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     }
 
     return RES_ERROR;
@@ -166,7 +329,7 @@ DRESULT disk_read(BYTE  pdrv,   /* Physical drive number (0..) */
 
 
 
-#if FF_FS_READONLY == 0
+#if (FF_FS_READONLY == 0)
 
 // -------- WRITE SECTOR(S) ---------------------------------------------------
 
@@ -175,7 +338,11 @@ DRESULT disk_write(      BYTE  pdrv,   /* Physical drive number (0..) */
                          DWORD sector, /* Sector address (LBA) */
                          UINT  count   /* Number of sectors to write (1..128) */)
 {
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     if(pdrv > 1 || count == 0)
+#else
+    if(pdrv > 0 || count == 0)
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     {
         return RES_PARERR;
     }
@@ -192,29 +359,27 @@ DRESULT disk_write(      BYTE  pdrv,   /* Physical drive number (0..) */
 
     switch(pdrv)
     {
-        case FLASH:
-            if(SpiNorFlash_write_sector(sector, buff, count) == true)
+        case SDCARD:
+            if(SpiSdCard_fatfs_write_sector(sector, buff, count) == true)
             {
                 return RES_OK;
             }
-            /*else if(SpiFlashAt45_WriteSector(sector, buff, count) == true)
-            {
-                return RES_OK;
-            }*/
             break;
 
-        case SDCARD:
-            if(SpiSdCard_write_sector(sector, buff, count) == true)
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
+        case FLASH:
+            if(SpiNorFlash_fatfs_write_sector(sector, buff, count) == true)
             {
                 return RES_OK;
             }
             break;
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     }
 
     return RES_ERROR;
 }
 
-#endif
+#endif // (FF_FS_READONLY == 0)
 
 
 
@@ -225,7 +390,11 @@ DRESULT disk_ioctl(BYTE  pdrv, /* Physical drive number (0..) */
                    BYTE  cmd,  /* Control code */
                    void *buff  /* Buffer to send/receive control data */)
 {
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     if(pdrv > 1)
+#else
+    if(pdrv > 0)
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     {
         return RES_PARERR;
     }
@@ -237,55 +406,37 @@ DRESULT disk_ioctl(BYTE  pdrv, /* Physical drive number (0..) */
 
     switch(pdrv)
     {
-        case FLASH:
-            if(SpiNorFlash_control(cmd, buff) == true)
-            {
-                if(cmd == CTRL_PROTECT)
-                {
-                    const bool protect = *(bool *)buff;
-
-                    if(protect == true)
-                    {
-                        // Write protected
-                        g_disk_status[FLASH] |= STA_PROTECT;
-                    }
-                    else
-                    {
-                        // Writable
-                        g_disk_status[FLASH] &= ~STA_PROTECT;
-                    }
-                }
-
-                return RES_OK;
-            }
-            /*else if(SpiFlashAt45_Control(cmd, buff) == true)
-            {
-                if(cmd == CTRL_PROTECT)
-                {
-                    const bool protect = *(bool *)buff;
-
-                    if(protect == true)
-                    {
-                        // Write protected
-                        g_disk_status[FLASH] |= STA_PROTECT;
-                    }
-                    else
-                    {
-                        // Writable
-                        g_disk_status[FLASH] &= ~STA_PROTECT;
-                    }
-                }
-
-                return RES_OK;
-            }*/
-            break;
-
         case SDCARD:
-            if(SpiSdCard_control(cmd, buff) == true)
+            if(SpiSdCard_fatfs_control(cmd, buff) == true)
             {
                 return RES_OK;
             }
             break;
+
+#if defined(FF_VOLUMES) && (FF_VOLUMES > 1)
+        case FLASH:
+            if(SpiNorFlash_fatfs_control(cmd, buff) == true)
+            {
+                if(cmd == CTRL_PROTECT)
+                {
+                    const bool protect = *(bool *)buff;
+
+                    if(protect == true)
+                    {
+                        // Write protected
+                        g_disk_status[FLASH] |= STA_PROTECT;
+                    }
+                    else
+                    {
+                        // Writable
+                        g_disk_status[FLASH] &= ~STA_PROTECT;
+                    }
+                }
+
+                return RES_OK;
+            }
+            break;
+#endif // defined(FF_VOLUMES) && (FF_VOLUMES > 1)
     }
 
     return RES_ERROR;
@@ -323,3 +474,5 @@ DWORD get_fattime(void)
 } // extern "C"
 
 } // namespace xarmlib
+
+#endif // (XARMLIB_ENABLE_FATFS == 1)
