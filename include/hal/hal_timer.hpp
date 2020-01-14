27f77e41cdcbcbad1a4c1752c617040e04d51a7f
emanuelpinto <emanuelangelopinto@gmail.com>
2020-01-14 14:55:26 +0000

    KV5x / Hal - added Timer driver

--- include/hal/hal_timer.hpp
+++ include/hal/hal_timer.hpp
@@ -1,11 +1,11 @@
 // ----------------------------------------------------------------------------
 // @file    hal_timer.hpp
 // @brief   Timer 32-bit HAL interface class.
-// @date    10 May 2019
+// @date    14 January 2020
 // ----------------------------------------------------------------------------
 //
 // Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
-// Copyright (c) 2018-2019 Helder Parracho (hparracho@gmail.com)
+// Copyright (c) 2018-2020 Helder Parracho (hparracho@gmail.com)
 //
 // See README.md file for additional credits and acknowledgments.
 //
@@ -98,7 +98,24 @@ class TimerBase : protected TimerDriver
 
 #include "core/target_specs.hpp"
 
-#if defined __KV4X__
+#if defined __KV5X__
+
+#include "targets/KV5x/kv5x_timer.hpp"
+
+namespace xarmlib
+{
+namespace hal
+{
+
+using Timer = TimerBase<targets::kv5x::TimerDriver>;
+
+} // namespace hal
+
+using Timer = hal::Timer;
+
+} // namespace xarmlib
+
+#elif defined __KV4X__
 
 #include "targets/KV4x/kv4x_timer.hpp"
 
