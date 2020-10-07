59a1f4a747af55fdb406f57877015a8736e42aa6
Helder Parracho <hparracho@gmail.com>
2020-10-07 12:06:16 +0100

    Update Os (support) class

--- include/core/os_support.hpp
+++ include/core/os_support.hpp
@@ -1,60 +1,48 @@
 // ----------------------------------------------------------------------------
 // @file    os_support.hpp
 // @brief   Operating System (FreeRTOS / baremetal) support functions.
-// @date    11 September 2020
+// @date    6 October 2020
 // ----------------------------------------------------------------------------
 //
-// Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
-// Copyright (c) 2020 Helder Parracho (hparracho@gmail.com)
+// Xarmlib 0.2.0 - https://github.com/hparracho/Xarmlib
+// Copyright (c) 2018-2020 Helder Parracho (hparracho@gmail.com)
+// PDX-License-Identifier: MIT License
 //
 // See README.md file for additional credits and acknowledgments.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-//
 // ----------------------------------------------------------------------------
 
-#ifndef __XARMLIB_CORE_OS_SUPPORT_HPP
-#define __XARMLIB_CORE_OS_SUPPORT_HPP
+#ifndef XARMLIB_CORE_OS_SUPPORT_HPP
+#define XARMLIB_CORE_OS_SUPPORT_HPP
 
-#include <cstdint>
+#include "xarmlib_config.hpp"
+
+
+
 
 namespace xarmlib
 {
 
-
-
-
 class Os
 {
-    public:
+public:
 
-        // --------------------------------------------------------------------
-        // PUBLIC MEMBER FUNCTIONS
-        // --------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // PUBLIC MEMBER FUNCTIONS
+    // ------------------------------------------------------------------------
 
-        // Provides context switch from an ISR handler (using FreeRTOS or baremetal)
-        static void yield_from_isr(const int32_t yield) noexcept;
+    // Provides context switch from an ISR handler (using FreeRTOS or baremetal)
+    static void yield_from_isr([[maybe_unused]] const int32_t yield) noexcept
+    {
+#if (XARMLIB_ENABLE_FREERTOS == 1)
+        portYIELD_FROM_ISR(yield);
+#endif
+    }
 };
-
-
-
 
 } // namespace xarmlib
 
-#endif // __XARMLIB_CORE_OS_SUPPORT_HPP
+
+
+
+#endif // XARMLIB_CORE_OS_SUPPORT_HPP
