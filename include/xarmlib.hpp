37ef6f4c6e201caf22dc1c85aec1b69029332cc4
Helder Parracho <hparracho@gmail.com>
2020-10-07 12:11:05 +0100

    Update xarmlib.hpp

--- include/xarmlib.hpp
+++ include/xarmlib.hpp
@@ -1,71 +1,69 @@
 // ----------------------------------------------------------------------------
 // @file    xarmlib.hpp
 // @brief   Xarmlib main header file.
-// @date    31 August 2020
+// @date    6 October 2020
 // ----------------------------------------------------------------------------
 //
-// Xarmlib 0.1.0 - https://github.com/hparracho/Xarmlib
+// Xarmlib 0.2.0 - https://github.com/hparracho/Xarmlib
 // Copyright (c) 2018-2020 Helder Parracho (hparracho@gmail.com)
+// PDX-License-Identifier: MIT License
 //
 // See README.md file for additional credits and acknowledgments.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-//
 // ----------------------------------------------------------------------------
 
-#ifndef __XARMLIB_HPP
-#define __XARMLIB_HPP
+#ifndef XARMLIB_HPP
+#define XARMLIB_HPP
 
 
 
 
 // API interface
+
 #include "api/api_crc.hpp"
 #include "api/api_digital_in.hpp"
 #include "api/api_digital_in_bus.hpp"
 #include "api/api_digital_out.hpp"
-#include "api/api_digital_out_bus.hpp"
-#include "api/api_input_debouncer.hpp"
-#include "api/api_io_debouncer.hpp"
-#include "api/api_output_driver.hpp"
+#include "api/api_high_res_clock.hpp"
+//#include "api/api_digital_out_bus.hpp"
+//#include "api/api_input_debouncer.hpp"
+//#include "api/api_io_debouncer.hpp"
+//#include "api/api_output_driver.hpp"
 #include "api/api_pin_bus.hpp"
-#include "api/api_pin_scanner.hpp"
+//#include "api/api_pin_scanner.hpp"
+
 
 // Targets HAL interface
-#include "hal/hal_can.hpp"
-#include "hal/hal_enc.hpp"
-#include "hal/hal_faim.hpp"
+
+//#include "hal/hal_can.hpp"
+//#include "hal/hal_enc.hpp"
+//#include "hal/hal_faim.hpp"
 #include "hal/hal_flash.hpp"
 #include "hal/hal_gpio.hpp"
-#include "hal/hal_i2c.hpp"
+//#include "hal/hal_i2c.hpp"
 #include "hal/hal_pin.hpp"
 #include "hal/hal_pin_int.hpp"
 #include "hal/hal_port.hpp"
-#include "hal/hal_spi.hpp"
+//#include "hal/hal_spi.hpp"
 #include "hal/hal_system.hpp"
 #include "hal/hal_timer.hpp"
-#include "hal/hal_timer16.hpp"
-#include "hal/hal_uart.hpp"
+#include "hal/hal_usart.hpp"
 #include "hal/hal_us_ticker.hpp"
 #include "hal/hal_watchdog.hpp"
 
 
+#include "core/target_flash.hpp"
+#include "core/target_gpio.hpp"
+#include "core/target_pin.hpp"
+#include "core/target_pin_int.hpp"
+#include "core/target_port.hpp"
+#include "core/target_specs.hpp"
+#include "core/target_system.hpp"
+#include "core/target_timer.hpp"
+#include "core/target_usart.hpp"
+#include "core/target_watchdog.hpp"
 
 
-#endif // __XARMLIB_HPP
+
+
+#endif // XARMLIB_HPP
